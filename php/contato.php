2ff36467b87977d30ac40696be516670d411034b
Edson S. Gomes <esg.bauru@gmail.com>
2021-09-13 18:17:27 -0300

    Adicionada a biblioteca PHPMailer

--- php/contato.php
+++ php/contato.php
@@ -1,24 +1,46 @@
 <?php
 
+    require_once("src/PHPMailer.php");
+    require_once("src/SMTP.php");
+    require_once("src/Exception.php");
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+
     if(isset($_POST[email]) && !empty($_POST[email])){
         $nome = addslashes($_POST['nome']);
         $fone = addslashes($_POST['fone']);
         $email = addslashes($_POST['email']);
-        $msg = addslashes($_POST['msg'])
+        $msg = addslashes($_POST['msg']);
 
-        $to = "informatica@brambilla.com.br";
-        $subject = "Contato - Site VDML";
-        $body = "Nome: " . $nome . "\r\n" .
-                "e-mail: " . $email . "\r\n" . 
-                "Mensagem: " . $msg;
-        $header = "From:endereço email" . "\r\n" . 
-                "Reply-To:" . $email . "\r\n" . 
-                "X=Mailer:PHP/" . phpversion();
+        $sendMail = new PHPMailer(true);
 
-        if(mail($to, $subject, $body, $header)){
-            echo("Mensagem enviada com sucesso.");
-        }else{
-            echo("Falha ao enviar a mensagem");
+        try {
+            $sendMail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $sendMail->isSMTP();
+            $sendMail->Host = "smtp.gmail.com";
+            $sendmail->SMTPAuth = true;
+            $sendMail->Username = "birigui.escolar@gmail.com";
+            $sendMail->Password = "bram2014";
+            $sendMail->Port = 587;
+
+            $sendMail->setFrom("birigui.escolar@gmail.com");
+            $sendMail->addAddress("informatica@brambilla.com.br");
+
+            $sendMail->isHTML(true);
+            $sendMail->Subject = "Contato VDML";
+            $sendMail->Body = "Mensagem de teste de <strong>TI Brambilla</strong>";
+            $sendMail->AltBody = "Mensagem de teste de TI Brambilla";
+
+            if($sendmail->send()){
+                echo "Sucesso";
+            }else{
+                echo "Não enviado"
+            }
+            
+        }catch (Exception $e){
+            echo ("Ocorreu o erro " . $e . " excessão {$sendMail->ErrorInfo}");
         }
     }
 ?>
